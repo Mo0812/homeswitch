50540227ae562b2bcca22e8945d64bc4c4fdc39c
mkanzler <moritzkanzler@googlemail.com>
2016-03-25 00:07:55 +0100

    Initial Commit

---
+++ homeswitch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  homeswitch
+//
+//  Created by Moritz Kanzler on 25.03.16.
+//  Copyright © 2016 Moritz Kanzler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ homeswitch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  homeswitch
+//
+//  Created by Moritz Kanzler on 25.03.16.
+//  Copyright © 2016 Moritz Kanzler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
